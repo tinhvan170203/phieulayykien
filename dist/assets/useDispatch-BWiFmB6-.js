0aa487c908381d741e79cfeb948b8632c8a321fd
Vu Van Tinh <vuvantinh121123@gmail.com>
2025-05-07 13:23:13 +0700

    new frontend

---
+++ dist/assets/useDispatch-BWiFmB6-.js
@@ -0,0 +1,1 @@
+import{aj as e,ak as u,al as c}from"./index-BAy75HH4.js";function r(t=e){const o=t===e?u:c(t);return function(){const{store:s}=o();return s}}const a=r();function i(t=e){const o=t===e?a:r(t);return function(){return o().dispatch}}const p=i();export{p as u};
